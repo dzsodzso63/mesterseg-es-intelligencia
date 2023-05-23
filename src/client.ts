a2c3576ba0c47752d43a12f057391b897a21ee33
Dzso Pengo <jozsef.pengo@formlabs.com>
2023-05-23 13:57:57 +0200

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -5,7 +5,9 @@ import rest from '@feathersjs/rest-client'
 const client = feathers()
 
 // Connect to a different URL
-const restClient = rest(`${window.location.protocol + "//" + window.location.hostname}:3030`);
+const restClient = rest(window.location.hostname.indexOf('joszaki.hu') > -1
+  ? 'https://mi-api.joszaki.hu'
+  : `${window.location.protocol + "//" + window.location.hostname}:3030`);
 
 // Configure an AJAX library (see below) with that client
 client.configure(restClient.fetch(window.fetch.bind(window)));
